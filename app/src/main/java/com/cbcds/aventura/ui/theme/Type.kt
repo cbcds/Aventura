02882b3b0d2babb6def8a5cf352b820a35b5cb87
Darya Sizova <darya.sizova31@gmail.com>
2022-06-19 18:24:31 +0300

    AVNT-0: initial commit

---
+++ app/src/main/java/com/cbcds/aventura/ui/theme/Type.kt
@@ -0,0 +1,123 @@
+package com.cbcds.aventura.ui.theme
+
+import androidx.compose.material3.Typography
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import com.cbcds.aventura.R
+
+val Inter = FontFamily(
+    Font(R.font.inter_regular, weight = FontWeight.Normal),
+    Font(R.font.inter_medium, weight = FontWeight.Medium),
+    Font(R.font.inter_bold, weight = FontWeight.Bold)
+)
+
+val AppTypography = Typography(
+    labelLarge = TextStyle(
+        fontFamily = Inter,
+        fontWeight = FontWeight.Normal,
+        letterSpacing = 0.1.sp,
+        lineHeight = 20.sp,
+        fontSize = 14.sp
+    ),
+    labelMedium = TextStyle(
+        fontFamily = Inter,
+        fontWeight = FontWeight.Medium,
+        letterSpacing = 0.5.sp,
+        lineHeight = 16.sp,
+        fontSize = 12.sp
+    ),
+    labelSmall = TextStyle(
+        fontFamily = Inter,
+        fontWeight = FontWeight.Medium,
+        letterSpacing = 0.5.sp,
+        lineHeight = 16.sp,
+        fontSize = 11.sp
+    ),
+    bodyLarge = TextStyle(
+        fontFamily = Inter,
+        fontWeight = FontWeight.Normal,
+        letterSpacing = 0.5.sp,
+        lineHeight = 24.sp,
+        fontSize = 16.sp
+    ),
+    bodyMedium = TextStyle(
+        fontFamily = Inter,
+        fontWeight = FontWeight.Normal,
+        letterSpacing = 0.25.sp,
+        lineHeight = 20.sp,
+        fontSize = 14.sp
+    ),
+    bodySmall = TextStyle(
+        fontFamily = Inter,
+        fontWeight = FontWeight.Normal,
+        letterSpacing = 0.4.sp,
+        lineHeight = 16.sp,
+        fontSize = 12.sp
+    ),
+    headlineLarge = TextStyle(
+        fontFamily = Inter,
+        fontWeight = FontWeight.Normal,
+        letterSpacing = 0.sp,
+        lineHeight = 40.sp,
+        fontSize = 32.sp
+    ),
+    headlineMedium = TextStyle(
+        fontFamily = Inter,
+        fontWeight = FontWeight.Normal,
+        letterSpacing = 0.sp,
+        lineHeight = 36.sp,
+        fontSize = 28.sp
+    ),
+    headlineSmall = TextStyle(
+        fontFamily = Inter,
+        fontWeight = FontWeight.Normal,
+        letterSpacing = 0.sp,
+        lineHeight = 32.sp,
+        fontSize = 24.sp
+    ),
+    displayLarge = TextStyle(
+        fontFamily = Inter,
+        fontWeight = FontWeight.Normal,
+        letterSpacing = (-0.25).sp,
+        lineHeight = 64.sp,
+        fontSize = 57.sp
+    ),
+    displayMedium = TextStyle(
+        fontFamily = Inter,
+        fontWeight = FontWeight.Normal,
+        letterSpacing = 0.sp,
+        lineHeight = 52.sp,
+        fontSize = 45.sp
+    ),
+    displaySmall = TextStyle(
+        fontFamily = Inter,
+        fontWeight = FontWeight.Normal,
+        letterSpacing = 0.sp,
+        lineHeight = 44.sp,
+        fontSize = 36.sp
+    ),
+    titleLarge = TextStyle(
+        fontFamily = Inter,
+        fontWeight = FontWeight.Normal,
+        letterSpacing = 0.sp,
+        lineHeight = 28.sp,
+        fontSize = 22.sp
+    ),
+    titleMedium = TextStyle(
+        fontFamily = Inter,
+        fontWeight = FontWeight.Medium,
+        letterSpacing = 0.15.sp,
+        lineHeight = 24.sp,
+        fontSize = 16.sp
+    ),
+    titleSmall = TextStyle(
+        fontFamily = Inter,
+        fontWeight = FontWeight.Medium,
+        letterSpacing = 0.1.sp,
+        lineHeight = 20.sp,
+        fontSize = 14.sp
+    )
+)
